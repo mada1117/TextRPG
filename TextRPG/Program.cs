c91faa0f40561c46f8b9a02247796a2128df2cfe
mada1117 <tkdwns1005@gmail.com>
2025-04-18 14:53:40 +0900

    Player1 클래스 작성, player 객체 생성

--- TextRPG/Program.cs
+++ TextRPG/Program.cs
@@ -1,10 +1,39 @@
 ﻿using System;
 using System.Numerics;
+using System.Runtime.InteropServices;
 
 namespace TextRPG
 {
-    internal class Program
+    class Player1
     {
+        public string Name { get; set; }
+        public string Job { get; set; } = "전사";
+        public int Level { get; set; } = 1;
+        public int Attack { get; set; } = 10;
+        public int Defense { get; set; } = 5;
+        public int Hp { get; set; } = 100;
+        public int Gold { get; set; } = 1500;
+
+        public Player1(string name)
+        {
+            Name = name;
+        }
+    }
+    class Item
+    {
+
+    }
+
+    class StoreItem
+    {
+
+    }
+
+
+    class Program
+    {
+        static Player1 player1;
+
         static void Main(string[] args)
         {
             // 게임 시작화면
@@ -22,8 +51,7 @@ namespace TextRPG
                 Console.WriteLine("\n여정을 시작할 당신의 이름은 무엇입니까?");
                 Console.Write(">> ");
                 string playerName = Console.ReadLine();
-                Console.Clear();
-                Console.WriteLine($"\n환영합니다, {playerName}님! 모험을 시작합니다...");
+                player1 = new Player1(playerName);
                 VillageUI();
             }
             else
@@ -69,8 +97,6 @@ namespace TextRPG
 
                         default:
                             Console.WriteLine("잘못된 입력입니다.");
-                            Console.WriteLine("\"계속하려면 아무 키나 누르세요...");
-                            Console.ReadKey();
                             break;
                     }
                 }
@@ -78,16 +104,52 @@ namespace TextRPG
 
             static void StatusUi()
             {
+                while (true)
+                {
+                    Console.Clear();
+                    Console.Title = "상태 보기";
+                    Console.WriteLine("\n상태 보기");
+                    Console.WriteLine("캐릭터의 정보가 표시됩니다.\n");
+                    // 상태 정보 출력
+                    Console.WriteLine($"Lv. {player1.Level:00}");
+                    Console.WriteLine($"이름: {player1.Name} ( 직업: {player1.Job} )");
+                    Console.WriteLine($"공격력 : {player1.Attack}");
+                    Console.WriteLine($"방어력 : {player1.Defense}");
+                    Console.WriteLine($"체 력 : {player1.Hp}");
+                    Console.WriteLine($"Gold : {player1.Gold} G");
+
+                    Console.WriteLine("\n0. 나가기");
+                    Console.WriteLine("\n원하시는 행동을 입력해주세요.");
+                    Console.Write(">> ");
+                    string input = Console.ReadLine();
+
+                    if (input == "0") break;
+
+                    Console.WriteLine("잘못된 입력입니다. 계속하려면 아무 키나 누르세요...");
+                    Console.ReadKey();
+                }
 
             }
 
             static void InventoryUi()
             {
-
+                Console.Clear();
+                Console.Title = "인벤토리";
+                Console.WriteLine("\n인벤토리");
+                Console.WriteLine("보유 중인 아이템을 관리할 수 있습니다.\n");
+                Console.WriteLine("[아이템 목록]");
             }
 
             static void StoreUi()
             {
+                Console.Clear();
+                Console.Title = "상점";
+                Console.WriteLine("\n상점");
+                Console.WriteLine("필요한 아이템을 얻을 수 있는 상점입니다.\n");
+
+                Console.WriteLine("[보유 골드]");
+
+                Console.WriteLine("[아이템 목록]");
 
             }
 
