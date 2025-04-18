ccf6dd5b08893a981fb03cc49d7e5bbae38b9747
mada1117 <tkdwns1005@gmail.com>
2025-04-18 10:59:21 +0900

    초기 커밋.

---
+++ TextRPG/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace TextRPG
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "스파르타 던젼";
+            Console.WriteLine("스파르타 던젼에 오신 여러분을 환영합니다.");
+            Console.WriteLine("원하시는 이름을 설정해주세요.");
+
+            Console.Write(">> ");
+            string playerName = Console.ReadLine();
+
+            Console.WriteLine($"\n환영합니다, {playerName}님! 모험을 시작합니다...");
+        }
+    }
+}
